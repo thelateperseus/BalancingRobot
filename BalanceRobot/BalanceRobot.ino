1053d2afc630774ba582378e86ac3b433f3ea444
Nathan C <1849077+thelateperseus@users.noreply.github.com>
2020-05-07 18:49:38 +1000

    Added yaw PID control
    Prior to this, the robot would tend to change heading as the wheels
    sometimes move independently. This additional PID control causes the
    robot to recenter to its original heading.

--- BalanceRobot/BalanceRobot.ino
+++ BalanceRobot/BalanceRobot.ino
@@ -24,12 +24,17 @@ Quaternion q;           // [w, x, y, z]         quaternion container
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-double setPoint = 0;
-double pitchAngle;
-double output;
+double pitchSetPoint = 0;
+double pitchReading;
+double pitchOutput;
 //PID pid(&pitchAngle, &output, &setPoint, 45, 200, 1.2, DIRECT);
 //PID pid(&pitchAngle, &output, &setPoint, 45, 300, 1, DIRECT);
-PID pid(&pitchAngle, &output, &setPoint, 45, 350, 1, DIRECT);
+PID pitchPid(&pitchReading, &pitchOutput, &pitchSetPoint, 45, 350, 1, DIRECT);
+
+double yawSetPoint = 0;
+double yawReading;
+double yawOutput;
+PID yawPid(&yawReading, &yawOutput, &yawSetPoint, 1, 0, 0, DIRECT);
 
 void setup() {
   Serial.begin(115200);
@@ -92,9 +97,12 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(ENB, OUTPUT);
 
-  pid.SetSampleTime(5);
-  pid.SetOutputLimits(-255, 255);
-  pid.SetMode(AUTOMATIC);
+  pitchPid.SetSampleTime(5);
+  pitchPid.SetOutputLimits(-255, 255);
+  pitchPid.SetMode(AUTOMATIC);
+  yawPid.SetSampleTime(5);
+  yawPid.SetOutputLimits(-10, 10);
+  yawPid.SetMode(AUTOMATIC);
 
   pinMode(LED_BUILTIN, OUTPUT);
   if (dmpReady) digitalWrite(LED_BUILTIN, HIGH);
@@ -112,35 +120,40 @@ void loop() {
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    //Serial.print("yaw:");
-    //Serial.print(ypr[0] * 180/M_PI);
+    yawReading = ypr[0] * 180/M_PI;
+    pitchReading = ypr[1] * 180/M_PI;
+
+    Serial.print("yaw:");
+    Serial.print(yawReading);
     Serial.print(", pitch:");
-    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print(pitchReading);
     //Serial.print(", roll:");
     //Serial.print(ypr[2] * 180/M_PI);
-    pitchAngle = ypr[1] * 180/M_PI;
 
-    pid.Compute();
+    pitchPid.Compute();
+    yawPid.Compute();
 
-    //Serial.print(", output:");
-    //Serial.print(output);
+    Serial.print(", yawOutput:");
+    Serial.print(yawOutput);
     Serial.println();
-    //delay(5);
+
+    double speedA = pitchOutput + yawOutput;
+    double speedB = pitchOutput - yawOutput;
+    if (pitchReading > 45 || pitchReading < -45) {
+      speedA = 0;
+      speedB = 0;
+    }
 
     //control speed, deadzone A:50, B:40
-    double outputA = map(abs(output),0,255,54,255);
-    double outputB = map(abs(output),0,255,40,255);
-    if (pitchAngle > 45 || pitchAngle < -45) {
-      outputA = 0;
-      outputB = 0;
-    }
-    analogWrite(ENA, outputA);
-    analogWrite(ENB, outputB);
+    double pwmA = map(abs(speedA),0,255,54,255);
+    double pwmB = map(abs(speedB),0,255,40,255);
+    analogWrite(ENA, pwmA);
+    analogWrite(ENB, pwmB);
     //control direction 
-    digitalWrite(IN1, output < 0 ? HIGH : LOW);
-    digitalWrite(IN2, output > 0 ? HIGH : LOW);
+    digitalWrite(IN1, speedA < 0 ? HIGH : LOW);
+    digitalWrite(IN2, speedA > 0 ? HIGH : LOW);
     //direction reversed for second motor (wiring)
-    digitalWrite(IN3, output < 0 ? LOW : HIGH);
-    digitalWrite(IN4, output > 0 ? LOW : HIGH);
+    digitalWrite(IN3, speedB < 0 ? LOW : HIGH);
+    digitalWrite(IN4, speedB > 0 ? LOW : HIGH);
   }
 }
