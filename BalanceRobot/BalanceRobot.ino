e89f98fa20a536de675e894c4f13a8b859a25ed5
Nathan C <1849077+thelateperseus@users.noreply.github.com>
2020-05-16 19:26:57 +1000

    Improved speed control with PID controller
    Added a second PID controller which measures speed and controls the pitch angle setpoint. This replaces both the previous balance setpoint code and the drive setpoint code. Now the remote command simply sets a target speed and the PID controller changes the pitch angle setpoint to achieve that speed.
    
    Further tuning of this PID controller is required, as is some filtering of the speed value to reduce noise.

--- BalanceRobot/BalanceRobot.ino
+++ BalanceRobot/BalanceRobot.ino
@@ -21,18 +21,20 @@ boolean started = false;
 
 long gyroXCalibration = 0, gyroYCalibration = 0;
 
-double pitchBalanceSetPoint = 0;
-double pitchDriveSetPoint = 0;
-
 double pitchSetPoint = 0;
 double pitchReading = 0;
-double pitchOutput = 0;
-PID pitchPid(&pitchReading, &pitchOutput, &pitchSetPoint, 30, 100, 1, REVERSE);
+double pitchPidOutput = 0;
+PID pitchPid(&pitchReading, &pitchPidOutput, &pitchSetPoint, 30, 100, 1, REVERSE);
+
+double speedSetPoint = 0;
+double speedPidOutput = 0;
+//PID speedPid(&pitchPidOutput, &speedPidOutput, &speedSetPoint, 0.00025, 0, 0.000005, DIRECT);
+PID speedPid(&pitchPidOutput, &speedPidOutput, &speedSetPoint, 0.00025, 0, 0.000002, DIRECT);
 /*
-double yawSetPoint = 0;
-double yawReading = 0;
-double yawOutput = 0;
-PID yawPid(&yawReading, &yawOutput, &yawSetPoint, 1, 0, 0, DIRECT);
+  double yawSetPoint = 0;
+  double yawReading = 0;
+  double yawOutput = 0;
+  PID yawPid(&yawReading, &yawOutput, &yawSetPoint, 1, 0, 0, DIRECT);
 */
 
 int activeCommand = -1;
@@ -76,9 +78,11 @@ void setup() {
 
   pitchPid.SetSampleTime(4);
   pitchPid.SetOutputLimits(-255, 255);
+  speedPid.SetSampleTime(4);
+  speedPid.SetOutputLimits(-15, 15);
   /*yawPid.SetSampleTime(5);
-  yawPid.SetOutputLimits(-10, 10);
-  yawPid.SetMode(AUTOMATIC);*/
+    yawPid.SetOutputLimits(-10, 10);
+    yawPid.SetMode(AUTOMATIC);*/
 
   Serial.println("Measuring gyro calibration values...");
   loopEndTime = micros() + LOOP_MICROS;
@@ -90,8 +94,8 @@ void setup() {
     Wire.write(0x43); // GYRO_XOUT[15:8]
     Wire.endTransmission();
     Wire.requestFrom(GYRO_ADDRESS, 4); // GYRO_XOUT[15:8], GYRO_XOUT[7:0], GYRO_YOUT[15:8], GYRO_YOUT[7:0]
-    gyroXCalibration += Wire.read()<<8 | Wire.read();
-    gyroYCalibration += Wire.read()<<8 | Wire.read();
+    gyroXCalibration += Wire.read() << 8 | Wire.read();
+    gyroYCalibration += Wire.read() << 8 | Wire.read();
     delayMicroseconds(loopEndTime - micros()); // Simulate the main program loop time
     loopEndTime = micros() + LOOP_MICROS;
   }
@@ -115,70 +119,69 @@ void loop() {
     yawSetPoint -= 15;
     Serial.print("yawSetPoint: ");
     Serial.println(yawSetPoint);
-  } else if (command == 'r' || command == 'R') {
+    } else if (command == 'r' || command == 'R') {
     yawSetPoint += 15;
     Serial.print("yawSetPoint: ");
     Serial.println(yawSetPoint);
-  } else*/ if (command == 'f' || command == 'F') {
+    } else*/ if (command == 'f' || command == 'F') {
     activeCommand = command;
     commandTimer = millis();
     Serial.print("command: ");
     Serial.println((char)command);
-    pitchDriveSetPoint += 0.75;
+    speedSetPoint = 40;
   } else if (command == 'b' || command == 'B') {
     activeCommand = command;
     commandTimer = millis();
     Serial.print("command: ");
     Serial.println((char)command);
-    pitchDriveSetPoint -= 0.75;
+    speedSetPoint = -40;
   }
 
   unsigned long commandTimeMillis = millis() - commandTimer;
   if (commandTimer > 0 && commandTimeMillis > 5000) {
     activeCommand = -1;
     commandTimer = 0;
-    pitchDriveSetPoint = 0;
+    speedSetPoint = 0;
     Serial.println("cleared command");
   }
 
   /* TODO handle yaw wrap-around somehow
-  while (yawSetPoint < -180) {
+    while (yawSetPoint < -180) {
     yawSetPoint += 360;
-  }
-  while (yawSetPoint > 180) {
+    }
+    while (yawSetPoint > 180) {
     yawSetPoint -= 360;
-  }*/
+    }*/
 
   // Angle calculations
   // ------------------
   /* http://www.geekmomprojects.com/gyroscopes-and-accelerometers-on-a-chip/
-   * Filtered Angle = α × (Gyroscope Angle) + (1 − α) × (Accelerometer Angle)
-   * where 
-   *     α = τ/(τ + Δt)   
-   *     (Gyroscope Angle) = (Last Measured Filtered Angle) + ω×Δt
-   *     Δt = sampling rate (4ms)
-   *     τ = time constant greater than timescale of typical accelerometer noise (1s)
-   */
+     Filtered Angle = α × (Gyroscope Angle) + (1 − α) × (Accelerometer Angle)
+     where
+         α = τ/(τ + Δt)
+         (Gyroscope Angle) = (Last Measured Filtered Angle) + ω×Δt
+         Δt = sampling rate (4ms)
+         τ = time constant greater than timescale of typical accelerometer noise (1s)
+  */
 
   // Read z accelerometer value
   Wire.beginTransmission(GYRO_ADDRESS);
   Wire.write(0x3F); // ACCEL_ZOUT[15:8]
   Wire.endTransmission();
   Wire.requestFrom(GYRO_ADDRESS, 2); // ACCEL_ZOUT[15:8], ACCEL_ZOUT[7:0]
-  long accelerometerZ = Wire.read()<<8 | Wire.read();
+  long accelerometerZ = Wire.read() << 8 | Wire.read();
   accelerometerZ += ACCELEROMETER_Z_CALIBRATION;
   accelerometerZ = constrain(accelerometerZ, -8200, 8200);
 
   // Calculate the current pitch angle in degrees according to the accelerometer
-  float pitchAccelerometer = asin((float)accelerometerZ/8200.0)* 57.296;
+  float pitchAccelerometer = asin((float)accelerometerZ / 8200.0) * 57.296;
 
   // Start balancing when angle is close to zero
-  if(!started && pitchAccelerometer > -0.5 && pitchAccelerometer < 0.5) {
+  if (!started && pitchAccelerometer > -0.5 && pitchAccelerometer < 0.5) {
     pitchReading = pitchAccelerometer;
     started = true;
-    pitchBalanceSetPoint = 0;
-    pitchDriveSetPoint = 0;
     pitchPid.SetMode(AUTOMATIC);
+    speedPid.SetMode(AUTOMATIC);
   }
 
   // Read X and Y gyro values
@@ -186,15 +189,15 @@ void loop() {
   Wire.write(0x43); // GYRO_XOUT[15:8]
   Wire.endTransmission();
   Wire.requestFrom(GYRO_ADDRESS, 4); // GYRO_XOUT[15:8], GYRO_XOUT[7:0], GYRO_YOUT[15:8], GYRO_YOUT[7:0]
-  long gyroX = Wire.read()<<8 | Wire.read();
-  long gyroY = Wire.read()<<8 | Wire.read();
-  
+  long gyroX = Wire.read() << 8 | Wire.read();
+  long gyroY = Wire.read() << 8 | Wire.read();
+
   gyroX -= gyroXCalibration;
   gyroY -= gyroYCalibration;
 
   // Calculate the angle in degrees traveled during this loop angle
   // (Gyroscope Angle) = (Last Measured Filtered Angle) + ω×Δt
-  float pitchGyro = pitchReading + gyroY * 0.000031;                             
+  float pitchGyro = pitchReading + gyroY * 0.000031;
 
   // Complementary filter to combine the gyro and accelerometer angle
   // Filtered Angle = α × (Gyroscope Angle) + (1 − α) × (Accelerometer Angle)
@@ -203,72 +206,42 @@ void loop() {
   // TODO compute accumulated Yaw
 
   if (started) {
-    pitchSetPoint = pitchBalanceSetPoint + pitchDriveSetPoint;
+    pitchSetPoint += speedPidOutput;
 
     pitchPid.Compute();
+    speedPid.Compute();
     //yawPid.Compute();
 
     //Serial.print("yaw:");
     //Serial.print(yawReading);
-    if (activeCommand != -1) {
-      /*Serial.print("bsp:");
-      Serial.print(pitchBalanceSetPoint);*/
-      Serial.print(", dsp:");
-      Serial.print(pitchDriveSetPoint);
-      /*Serial.print(", sp:");
-      Serial.print(pitchSetPoint);*/
-      Serial.print(", p:");
-      Serial.print(pitchReading);
-      Serial.print(", o:");
-      Serial.print(pitchOutput);
-      Serial.println();
-    }
 
-    // Deadband to prevent oscillations
-    if (pitchOutput > 0 && pitchOutput < 2) {
-      pitchOutput = 0;
-    }
-    if (pitchOutput < 0 && pitchOutput > -2) {
-      pitchOutput = 0;
-    }
+    Serial.print("psp:");
+    Serial.print(pitchSetPoint);
+    Serial.print(", p:");
+    Serial.print(pitchReading);
+    Serial.print(", ssp:");
+    Serial.print(speedSetPoint);
+    Serial.print(", s:");
+    Serial.print(pitchPidOutput);
+    Serial.println();
 
-    if (activeCommand == 'f' || activeCommand == 'F') {
-      if (commandTimeMillis > 1000) {
-        if (pitchOutput < DRIVE_SPEED_MIN) {
-          pitchDriveSetPoint += PITCH_DRIVE_SP_STEP;
-        }
-        if (pitchOutput > DRIVE_SPEED_MAX) {
-          pitchDriveSetPoint -= PITCH_DRIVE_SP_STEP * 20;
-        }
+    /*
+      // Deadband to prevent oscillations
+      if (pitchPidOutput > 0 && pitchPidOutput < 2) {
+      pitchPidOutput = 0;
       }
-    } else if (activeCommand == 'b' || activeCommand == 'B') {
-      if (commandTimeMillis > 1000) {
-        if (pitchOutput > -DRIVE_SPEED_MIN) {
-          pitchDriveSetPoint -= PITCH_DRIVE_SP_STEP;
-        }
-        if (pitchOutput < -DRIVE_SPEED_MAX) {
-          pitchDriveSetPoint += PITCH_DRIVE_SP_STEP * 20;
-        }
+      if (pitchPidOutput < 0 && pitchPidOutput > -2) {
+      pitchPidOutput = 0;
       }
-    } else if (pitchDriveSetPoint == 0) {
-      // The self balancing point is adjusted when the remote control isn't trying to move the robot.
-      // This should stop the robot from wandering.
-      if (pitchOutput < 0) {
-        pitchBalanceSetPoint += PITCH_BALANCE_SP_STEP;
-      }
-      if (pitchOutput > 0) {
-        pitchBalanceSetPoint -= PITCH_BALANCE_SP_STEP;
-      }
-    }
-    // Prevent runaway
-    pitchDriveSetPoint = constrain(pitchDriveSetPoint, -1.2, 1.2);
+    */
 
-    double speedA = pitchOutput;// + yawOutput;
-    double speedB = pitchOutput;// - yawOutput;
+    double speedA = pitchPidOutput;// + yawOutput;
+    double speedB = pitchPidOutput;// - yawOutput;
     if (pitchReading > 45 || pitchReading < -45) {
       speedA = 0;
       speedB = 0;
-      pitchDriveSetPoint = 0;
+      pitchSetPoint = 0;
+      speedSetPoint = 0;
       activeCommand = -1;
     }
 
@@ -276,11 +249,11 @@ void loop() {
     speedB = constrain(speedB, -255, 255);
 
     // control speed, deadzone A:50, B:40
-    double pwmA = map(abs(speedA),1,255,54,255);
-    double pwmB = map(abs(speedB),1,255,40,255);
+    double pwmA = map(abs(speedA), 1, 255, 54, 255);
+    double pwmB = map(abs(speedB), 1, 255, 40, 255);
     analogWrite(ENA, pwmA);
     analogWrite(ENB, pwmB);
-    //control direction 
+    //control direction
     digitalWrite(IN1, speedA < 0 ? HIGH : LOW);
     digitalWrite(IN2, speedA > 0 ? HIGH : LOW);
     //direction reversed for second motor (wiring)
